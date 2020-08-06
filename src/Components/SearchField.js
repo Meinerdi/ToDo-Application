64eeaf8ee341c8b1b7e985ef9d3a0057694b5382
Meinerdi <meinerdi@gmail.com>
2020-08-06 13:54:46 +0300

    v1.0

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react'
 
 export class SearchField extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            valueOfSearchTextField: "",
+            valueOfSearchDateField: ""
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            valueOfSearchTextField: localStorage.getItem("valueOfSearchText"),
+            valueOfSearchDateField: localStorage.getItem("valueOfSearchDate")
+        })
+    }
 
     handleSubmit = e => {
         e.preventDefault()
     }
 
     handleSearchText = e => {
+        this.setState({
+            valueOfSearchTextField: e.target.value
+        })
+
         this.props.onSearchByText(e.target.value)
         localStorage.setItem("valueOfSearchText", e.target.value)
     }
 
     handleSearchDate = e => {
+        this.setState({
+            valueOfSearchDateField: e.target.value
+        })
+
         this.props.onSearchByDate(e.target.value)
         localStorage.setItem("valueOfSearchDate", e.target.value)
     }
@@ -24,11 +47,13 @@ export class SearchField extends Component {
                     type="search" 
                     onChange={this.handleSearchText}
                     placeholder="Search here..."
+                    value={this.state.valueOfSearchTextField}
                 />
                 
                 <input 
                     type="date" 
                     onChange={this.handleSearchDate}
+                    value={this.state.valueOfSearchDateField}
                 />
 
             </form>
